b0e54429a336942aeeb3d7dc75d7968dce869dc6
ChrisJonesTC <cfjones91@gmail.com>
2022-09-14 10:54:25 -0400

    initial/final commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OperatorExercise;
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        
+        
+        
+        
+        
+        
+        
+        {
+            //Exercise 1:
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            //
+            if (a == 17 && b == 4)
+
+                //String Concatination
+                //Console.WriteLine(a + " / " + b + "is " + quotient + "remainder" + remainder);
+
+                //String Interprolation
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+        }
+         
+        
+    }
+}
